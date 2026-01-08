2adb45bdbd533d3fe989bec99c81afe9d22b4d54
Nganga02 <nganganicholas50@gmail.com>
2026-01-08 12:38:41 +0300

    feat: daily order reminders

--- crm/cron_jobs/clean_inactive_customers.sh
+++ crm/cron_jobs/clean_inactive_customers.sh
@@ -4,6 +4,6 @@ cd $PROJECT_DIR || exit 1
 #If using a virtual environment
 source $PROJECT_DIR/.venv/bin/activate
 
-DELETED_COUNT=$(python3 manage.py customer_cleanup.)
+DELETED_COUNT=$(python3 manage.py customer_cleanup)
 
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Deleted customers: $DELETED_COUNT" >> /tmp/customer_cleanup_log.txt
---
+++ crm/cron_jobs/send_order_reminders.py
@@ -0,0 +1,50 @@
+from gql import gql, Client
+from gql.transport.requests import RequestsHTTPTransport
+from django.utils import timezone 
+from datetime import timedelta, datetime
+
+transport = RequestsHTTPTransport(
+    url='http://localhost:8000/graphql',
+)
+
+
+client = Client(transport=transport)
+timeframe=timezone.now()-timedelta(days=7)
+
+
+
+query=gql(
+    """
+    query($after: Datetime!){
+        allOrders(order_date_after: $after){
+        edges{
+            node{
+                id
+                customer{
+                    email
+                    }
+                }
+            }
+        }
+    }
+"""
+)
+
+variables = {
+    "after": timeframe.isoformat()
+}
+
+result=client.execute(query, variable_values=variables)
+print(result)
+
+log_path = "/tmp/order_reminders_log.txt"
+timestamp = datetime.now().isoformat()
+
+with open(log_path, "a") as log_file:
+    for edge in result.get("allOrders", {}).get("edges", []):
+        order = edge.get("node", {})
+        order_id = order.get("id")
+        email = order.get("customer", {}).get("email")
+        log_file.write(f"[{timestamp}] Order ID: {order_id}, Customer Email: {email}\n")
+
+print("Order reminders processed!")
